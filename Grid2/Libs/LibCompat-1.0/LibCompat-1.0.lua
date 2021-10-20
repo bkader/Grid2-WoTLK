0ba42de497f87b57f3cb8db76b245f010f323ba4
Kader Bouyakoub <bkader@mail.com>
2021-10-20 19:56:01 +0100

    Update LibCompat-1.0.lua

--- Grid2/Libs/LibCompat-1.0/LibCompat-1.0.lua
+++ Grid2/Libs/LibCompat-1.0/LibCompat-1.0.lua
@@ -69,6 +69,77 @@ do
 end
 
 -------------------------------------------------------------------------------
+-- Lua Memoize
+
+do
+	local unpack = unpack
+	local getmetatable = getmetatable
+	local memoizedFunc = {}
+
+	local function isCallable(func)
+		-- function or method?
+		if type(func) == "function" then
+			return true
+		end
+		-- maybe a metatable.
+		if type(func) == "table" then
+			local mt = getmetatable(func)
+			return (type(mt) == "table" and isCallable(mt.__call))
+		end
+		return false
+	end
+
+	local function cacheGet(cache, params)
+		local node = cache
+		for i = 1, #params do
+			node = node.children and node.children[params[i]]
+			if not node then
+				return nil
+			end
+		end
+		return node.results
+	end
+
+	local function cachePut(cache, params, results)
+		local node = cache
+		local i = 1
+		local param = params[i]
+		while param do
+			node.children = node.children or {}
+			node.children[param] = node.children[param] or {}
+			node = node.children[param]
+			i = i + 1
+			param = params[i]
+		end
+		node.results = results
+	end
+
+	local function memoize(func, cache)
+		if not isCallable(func) then
+			error(("Only functions and callable tables are memoizable. Received %s (a %s)"):format(tostring(func), type(func)), 2)
+		end
+
+		cache = cache or memoizedFunc[func]
+		if not cache then
+			memoizedFunc[func] = {}
+			cache = memoizedFunc[func]
+		end
+
+		return function(...)
+			local params = {...}
+			local results = cacheGet(cache, params)
+			if not results then
+				results = {func(...)}
+				cachePut(cache, params, results)
+			end
+			return unpack(results)
+		end
+	end
+
+	lib.memoize = memoize
+end
+
+-------------------------------------------------------------------------------
 
 do
 	local pcall = pcall
@@ -1615,6 +1686,8 @@ local mixins = {
 	"WeakTable",
 	"newTable",
 	"delTable",
+	-- lua memoize
+	"memoize",
 	-- math util
 	"Lerp",
 	"Round",
